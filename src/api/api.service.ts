0bda52a931f5360e6ef9d7602c30fc8d9d049eb3
Rémi Daniel <remi.daniel@insa-rennes.fr>
2021-07-08 17:54:12 +0200

    feat(logs): all logs are now kept in storage

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -4,14 +4,43 @@ import { ErrorDto } from './dto/error.dto';
 import * as fs from 'fs';
 
 const path = './logs/';
+const usageLogsName = 'usage.json';
+const errorLogsName = 'error.json';
 
 @Injectable()
 export class ApiService {
+  private usageLogs: Array<UsageDto> = [];
+  private errorLogs: Array<ErrorDto> = [];
+
+  constructor() {
+    this.loadLogData();
+  }
+
+  /**
+   * Loads the logs from previous sessions.
+   */
+  loadLogData() {
+    fs.mkdirSync(path, { recursive: true }); // Create logs directory if needed
+    try {
+      const data = fs.readFileSync(path + usageLogsName);
+      this.usageLogs = JSON.parse(data.toString());
+    } catch (err) {} // File did not exist, no data to load
+
+    try {
+      const data = fs.readFileSync(path + errorLogsName);
+      this.errorLogs = JSON.parse(data.toString());
+    } catch (err) {} // File did not exist, no data to load
+  }
+
+  /**
+   * Saves the provided usage data to the usage log file.
+   * @param usageDto The data to save.
+   */
   async saveUsageData(usageDto: UsageDto) {
     console.log('Received usage data: ' + usageDto.id + ' at ' + usageDto.date);
-    await fs.promises.mkdir(path, { recursive: true }); // Create logs directory if needed
-    const data = JSON.stringify(usageDto, null, 4);
-    fs.writeFile(path + 'usage.json', data, (err) => {
+    this.usageLogs.push(usageDto);
+    const data = JSON.stringify(this.usageLogs, null, 4);
+    fs.writeFile(path + usageLogsName, data, (err) => {
       if (err) {
         // If an error occurred while writing the file
         throw err;
@@ -19,13 +48,17 @@ export class ApiService {
     });
   }
 
+  /**
+   * Saves the provided error data to the error log file.
+   * @param errorDto The data to save.
+   */
   async saveErrorData(errorDto: ErrorDto) {
     console.log(
       'Received error data: ' + errorDto.msg + ' at ' + errorDto.date,
     );
-    await fs.promises.mkdir(path, { recursive: true }); // Create logs directory if needed
-    const data = JSON.stringify(errorDto, null, 4);
-    fs.writeFile(path + 'error.json', data, (err) => {
+    this.errorLogs.push(errorDto);
+    const data = JSON.stringify(this.errorLogs, null, 4);
+    fs.writeFile(path + errorLogsName, data, (err) => {
       if (err) {
         // If an error occurred while writing the file
         throw err;
